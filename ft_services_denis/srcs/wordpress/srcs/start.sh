177c421878832aef203b9b6b54e940152d6506ee
Armand Annara <arannara@il-h1.msk.21-school.ru>
2020-11-30 23:32:11 +0300

    baza zarabotala

--- ft_services_denis/srcs/wordpress/srcs/start.sh
+++
@@ -1,9 +0,0 @@
-#!bin/sh
-
-rc default
-
-mkdir /run/nginx
-
-chmod 744 /run/nginx
-
-/usr/bin/supervisord -c /etc/supervisord.conf
--- setup.sh
+++ setup.sh
@@ -3,17 +3,18 @@ minikube start --vm-driver=virtualbox
 eval $(minikube docker-env)
 
 minikube addons enable metallb
-kubectl apply -f metallb.yaml
+kubectl apply -f ./srcs/nginx/metallb.yaml
 
-# build nginx
-docker build -t nginx .
-# apply rule nginx
-kubectl apply -f nginx.yaml
+# NGINX
+docker build -t nginx ./srcs/nginx/
+kubectl apply -f ./srcs/nginx/nginx.yaml
 
-kubectl get svc
+# MYSQL
+docker build -t mysql ./srcs/mysql/
+kubectl apply -f ./srcs/mysql/mysql-volume.yaml
+kubectl apply -f ./srcs/mysql/mysql.yaml
 
 minikube dashboard
-
 
 # kubectl exec deploy/nginx -- pkill nginx
 # kubectl exec deploy/nginx -- pkill sshd
