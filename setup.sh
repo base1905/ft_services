a741ee093889d37465486408ddaab2bd080f9bdd
Armand Annara <arannara@il-h3.msk.21-school.ru>
2020-12-06 20:26:23 +0300

    new ideas

--- setup.sh
+++ setup.sh
@@ -38,3 +38,16 @@ kubectl apply -f ./srcs/telegraf/
 #GRAFANA
 docker build -t grafana ./srcs/grafana
 kubectl apply -f ./srcs/grafana/
+
+
+# kubectl exec deploy/ftps -- pkill vsftpd
+# kubectl exec deploy/grafana -- pkill grafana
+# kubectl exec deploy/telegraf -- pkill telegraf
+# kubectl exec deploy/influxdb -- pkill influx
+# kubectl exec deploy/wordpress -- pkill nginx
+# kubectl exec deploy/wordpress -- pkill php-fpm7
+# kubectl exec deploy/phpmyadmin -- pkill nginx
+# kubectl exec deploy/phpmyadmin -- pkill php-fpm7
+# kubectl exec deploy/mysql -- pkill /usr/bin/mysqld 
+# kubectl exec deploy/nginx -- pkill nginx
+# kubectl exec deploy/nginx -- pkill ssh
